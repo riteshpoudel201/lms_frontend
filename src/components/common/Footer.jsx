cbff681564a4b6f429c617c3bf4255c6bb5fc9be
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-01 19:29:12 +0545

    Created the footer and displayed simple copyright message

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,10 @@
+Animation
+const Footer = () => {
+  return (
+    <div className="bg-dark text-white p-4 text-center">
+        &copy; Copyright All rights reserved! Made by Ritesh Poudel.
+    </div>
+  )
+}
+
+export default Footer
