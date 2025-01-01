94a524a643371115d26726aca397d82a7d98431e
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-01 19:28:10 +0545

    Removed testing buttons and toast and imported page routing configuration file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { Button } from "react-bootstrap";
-import { FaBacon } from "react-icons/fa6";
-import { toast, ToastContainer } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import AppRoutes from "./routes/AppRoutes";
 
 const App = () => {
   return (
     <>
-      <div>hello</div>
-      <Button onClick={()=> toast.success("Hellow")}><FaBacon />Button</Button>
+      <AppRoutes />
       <ToastContainer />
     </>
   );
