e09bb81f098751388f95a600c17bf37a499fd4c1
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-01 19:35:32 +0545

    routes are properly setup for all the required pages for this project

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,47 @@
+import { Route, Routes } from "react-router-dom";
+import {
+  HomePage,
+  DashboardPage,
+  UserPage,
+  SignInPage,
+  SignUpPage,
+  ForgetPassword,
+  Books,
+  NewBookPage,
+  EditBookPage,
+  BookLandingPage,
+  ReviewsPage,
+  BorrowPage,
+  ProfilePage,
+} from "@pages";
+import DefaultLayout from "@components/layouts/DefaultLayout";
+import UserLayout from "@components/layouts/UserLayout";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      {/* public routes  */}
+      <Route path="/" element={<DefaultLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="signin" element={<SignInPage />} />
+        <Route path="signup" element={<SignUpPage />} />
+        <Route path="forget" element={<ForgetPassword />} />
+      </Route>
+
+      {/* private routes  */}
+      <Route path="/user" element={<UserLayout />}>
+        <Route index element={<DashboardPage />} />
+        <Route path="users" element={<UserPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="books" element={<Books />} />
+        <Route path="new-book" element={<NewBookPage />} />
+        <Route path="edit-book" element={<EditBookPage />} />
+        <Route path="book-landing" element={<BookLandingPage />} />
+        <Route path="reviews" element={<ReviewsPage />} />
+        <Route path="borrow" element={<BorrowPage />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
