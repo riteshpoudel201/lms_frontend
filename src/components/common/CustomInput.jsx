b426c678483a9023ec51d7672453ef72fce3d4a3
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-06 21:10:36 +0545

    added button to show password when clicked

--- src/components/common/CustomInput.jsx
+++ src/components/common/CustomInput.jsx
@@ -1,13 +1,38 @@
 /* eslint-disable react/prop-types */
 import Form from "react-bootstrap/Form";
-const CustomInput = ({ label, name, text, required, ...rest }) => {
+import { EyeIcon } from "lucide-react";
+import { useState } from "react";
+const CustomInput = ({ label, name, text, required, type, ...rest }) => {
+  const [inputType, setType] = useState(type);
+  const handleEyeClick = () => {
+    setType("text");
+  };
   return (
     <Form.Group className="mb-3" controlId={name}>
       <Form.Label className="fs-6">
         {label}
         {required && <span>*</span>}
       </Form.Label>
-      <Form.Control name={name} required={required} {...rest} />
+      <div style={{ position: "relative" }}>
+        <Form.Control
+          type={inputType}
+          name={name}
+          required={required}
+          {...rest}
+        />
+        {type === "password" && (
+          <EyeIcon
+            onClick={handleEyeClick}
+            style={{
+              position: "absolute",
+              zIndex: "10",
+              right: "0.5rem",
+              top: "20%",
+              color: "gray",
+            }}
+          />
+        )}
+      </div>
       {text && <Form.Text className="text-muted">{text}</Form.Text>}
     </Form.Group>
   );
