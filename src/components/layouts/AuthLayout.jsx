30e1ba4b44471609ee5ffee0bc1141cacd3cac4a
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-10 17:10:59 +0545

    checking if user is logged in or not

--- src/components/layouts/AuthLayout.jsx
+++ src/components/layouts/AuthLayout.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/prop-types */
 
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
-const AuthLayout = ({children}) => {
-    const isAuth = true;
-  return isAuth ? children : <Navigate to="/signin" />
-}
+const AuthLayout = ({ children }) => {
+  const { user } = useSelector((state) => state.userInfo);
+  const isAuth = user?._id;
+  return isAuth ? children : <Navigate to="/signin" />;
+};
 
-export default AuthLayout
+export default AuthLayout;
