a66008a26da9f231e5f422858a2847db3b03c519
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-14 19:20:24 +0545

    added method for autologin which is then called in signin page

--- src/features/users/userAction.js
+++ src/features/users/userAction.js
@@ -1,3 +1,4 @@
+import { renewUserToken } from "../../services/authService";
 import { fetchUserApi } from "./userApi";
 import { setUsers } from "./userSlice";
 
@@ -7,4 +8,23 @@ export const fetchUserAction = () => async (dispatch) => {
   //receive user
   // dispatch user to redux store
   status === "success" && data?._id && dispatch(setUsers(data));
+};
+
+export const autoLoginUser =  () => async (dispatch) => {
+  const accessToken = sessionStorage.getItem("accessToken");
+  if (accessToken) {
+    dispatch(fetchUserAction());
+    return;
+  }
+  const refreshToken = localStorage.getItem("refreshToken");
+  if (refreshToken) {
+    const result = await renewUserToken();
+    console.log(result);
+    
+    if(result?.data){
+      sessionStorage.setItem("accessToken", result?.data);
+      dispatch(fetchUserAction())
+    }
+  }
+ 
 };
