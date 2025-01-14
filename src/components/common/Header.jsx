f012618a09f89c555591ac65d0eb40d17f63471c
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-14 19:20:40 +0545

    added logout method

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,10 +4,25 @@ import { FaHome, FaSignInAlt } from "react-icons/fa";
 import { HiOutlineLogin } from "react-icons/hi";
 import { MdDashboard } from "react-icons/md";
 import { PiSignOutBold } from "react-icons/pi";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import { signOutUser } from "@services/authService";
+import { toast } from "react-toastify";
+import { setUsers } from "@features/users/userSlice";
 const Header = () => {
   const { user } = useSelector((state) => state.userInfo);
+  const dispatch = useDispatch();
+
+  const handleLogout =async () => {
+    //logout from backend via api
+    const {status, message} = await signOutUser();
+    if(status === "success"){
+      toast[status](message);
+      sessionStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+      dispatch(setUsers({}))
+    }
+  }
   return (
     <Navbar expand="lg" className="bg-dark" variant="dark">
       <Container>
@@ -25,19 +40,23 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-          <NavLink className="nav-link" to="/">
-                  <FaHome />
-                  Home
-                </NavLink>
+            <NavLink className="nav-link" to="/">
+              <FaHome />
+              Home
+            </NavLink>
             {user._id ? (
               <>
-                <NavLink className="nav-link" to="/user"><MdDashboard className="me-2" />Dashboard</NavLink>
-                <NavLink className="nav-link" to="/user"><PiSignOutBold className="me-2" />Logout</NavLink>
-                
+                <NavLink className="nav-link" to="/user">
+                  <MdDashboard className="me-2" />
+                  Dashboard
+                </NavLink>
+                <NavLink className="nav-link" to="/" onClick={handleLogout}>
+                  <PiSignOutBold className="me-2" />
+                  Logout
+                </NavLink>
               </>
             ) : (
               <>
-                
                 <NavLink className="nav-link" to="/signin">
                   <HiOutlineLogin />
                   Login
