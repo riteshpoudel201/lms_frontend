06411addcfc1d11980b708026568dedbaa87f05d
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-17 10:54:42 +0545

    created the container for otp request form and password reset form

--- src/pages/auth/ForgetPassword.jsx
+++ src/pages/auth/ForgetPassword.jsx
@@ -1,7 +1,20 @@
+import { Card } from "react-bootstrap"
+import { Outlet } from "react-router-dom"
 
 const ForgetPassword = () => {
+  
+
   return (
-    <div>ForgetPassword</div>
+    <div className="vh-100 d-flex justify-content-center align-items-center my-4">
+    <Card style={{ width: "20rem", borderRadius: "10px" }}>
+        <Card.Title style={{ textAlign: "center", marginTop: "0.9rem" }}>
+          Forget Password?
+        </Card.Title>
+        <Card.Body>
+          <Outlet />
+        </Card.Body>
+      </Card>
+      </div>
   )
 }
 
