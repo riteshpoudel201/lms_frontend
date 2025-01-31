16fe203fd0b3f36ae5e344f73823285db6451ca4
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-31 13:38:59 +0545

    modified the import path of bookSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/users/userSlice"
-import bookReducer from "../features/users/bookSlice"
+import bookReducer from "../features/books/bookSlice"
 export default configureStore({
     reducer:{
         userInfo: userReducer ,
