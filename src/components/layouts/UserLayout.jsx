4941b17358f5860b06b38b23b99a24603c7a3d84
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-31 16:09:11 +0545

    fixed sidebar not staying on left in mobile screen

--- src/components/layouts/UserLayout.jsx
+++ src/components/layouts/UserLayout.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../common/Footer";
 import Header from "../common/Header";
-import { Col, Container, Row } from "react-bootstrap";
 import Sidebar from "../common/Sidebar";
 import AuthLayout from "./AuthLayout";
 
@@ -9,23 +8,17 @@ const UserLayout = () => {
   return (
     <AuthLayout>
       <Header />
-      <Container fluid>
-        <Row>
-          <Col md={3} xl={2} className="bg-dark text-white">
-            <div className="p-3">
-              <div>Welcome Back</div>
-              <h4>Ritesh Poudel</h4>
-              <hr />
-              <Sidebar />
-            </div>
-          </Col>
-          <Col md={9} xl={10}>
-            <main className="main">
-              <Outlet />
-            </main>
-          </Col>
-        </Row>
-      </Container>
+      <div className="w-100 d-flex flex-row">
+        <div className="p-3 bg-dark text-white" style={{ width: "15vw" }}>
+          <div>Welcome Back</div>
+          <h4>Ritesh Poudel</h4>
+          <hr />
+          <Sidebar />
+        </div>
+        <main className="main">
+          <Outlet />
+        </main>
+      </div>
 
       <Footer />
     </AuthLayout>
