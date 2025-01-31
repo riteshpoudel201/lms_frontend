b25ec53bd36be322862349f1be36141f4bc3ba50
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-31 16:08:47 +0545

    fixed the issue of always redirecting to dashboard page when opening on new tab

--- src/components/layouts/AuthLayout.jsx
+++ src/components/layouts/AuthLayout.jsx
@@ -2,11 +2,17 @@
 
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const AuthLayout = ({ children }) => {
+  const location = useLocation();
   const { user } = useSelector((state) => state.userInfo);
   const isAuth = user?._id;
-  return isAuth ? children : <Navigate to="/signin" />;
+  return isAuth ? (
+    children
+  ) : (
+    <Navigate state={{ from: location.pathname }} to="/signin" />
+  );
 };
 
 export default AuthLayout;
