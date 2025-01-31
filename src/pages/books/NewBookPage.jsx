5125a99d0fecbb115ebc8666845699a8c2255745
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-31 16:09:56 +0545

    added new book form

--- src/pages/books/NewBookPage.jsx
+++ src/pages/books/NewBookPage.jsx
@@ -1,7 +1,11 @@
+import { NewBookForm } from "@components/forms"
 
 const NewBookPage = () => {
   return (
-    <div>NewBookPage</div>
+    <div className="m-2">
+      <h3 className="mb-4">Insert New Book Details</h3>
+      <NewBookForm />
+    </div>
   )
 }
 
