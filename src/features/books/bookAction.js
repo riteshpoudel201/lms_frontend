95333a253fe89c20a2ab1932311a30452980f6db
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-02 17:10:36 +0545

    added action for posting the new book via api

--- src/features/books/bookAction.js
+++ src/features/books/bookAction.js
@@ -1,4 +1,4 @@
-import { fetchAllBookApi, fetchAvailableBookApi } from "./bookApi";
+import { fetchAllBookApi, fetchAvailableBookApi, postNewBookApi } from "./bookApi";
 import { setBooks } from "./bookSlice";
 
 
@@ -12,6 +12,14 @@ export const fetchAllBookAction = () => async (dispatch) => {
 export const fetchAvailableBookAction = () => async (dispatch) => {
   // call api
   const { status, data } = await fetchAvailableBookApi();
+  //receive book
+  // dispatch book to redux store
+  status === "success" && data?._id && dispatch(setBooks(data));
+};
+
+export const postNewBookAction = (payload) => async (dispatch) => {
+  // call api
+  const { status, data } = await postNewBookApi(payload);
   //receive book
   // dispatch book to redux store
   status === "success" && data?._id && dispatch(setBooks(data));
