c731158263eccea7fb0a8b76b7e856cc57f23300
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-02 17:11:07 +0545

    added action to submit function of the new book form

--- src/components/forms/books/NewBookForm.jsx
+++ src/components/forms/books/NewBookForm.jsx
@@ -2,30 +2,33 @@ import CustomInput from "@components/common/CustomInput";
 import useForm from "@hooks/useForm";
 import { Button, Form } from "react-bootstrap";
 import { bookFields } from "@/assets/custom-inputs/bookInputs";
-
-
+import { postNewBookAction } from "@features/books/bookAction";
+import { useDispatch } from "react-redux";
 
 const NewBookForm = () => {
-  const {formData, handleChange, isLoading, setIsLoading} = useForm({});
+  const { formData, handleChange, isLoading, setIsLoading } = useForm({});
 
-  const handleSubmit =(e) =>{
+  const dispatch = useDispatch();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
     console.log(formData);
+    const response = await dispatch(postNewBookAction(formData));
+    console.log(response);
     setIsLoading(false);
-  }
+  };
   return (
     <Form onSubmit={handleSubmit}>
-      {bookFields && bookFields.map((field) => (
-        <CustomInput
-          key={field.name}
-          onChange={handleChange}
-          {...field}
-        />
-      ))}
-   <div className="d-grid">
-    <Button type="submit" disabled={isLoading}>{isLoading ? "Adding book..." : "Add Book"}</Button>
-   </div>
+      {bookFields &&
+        bookFields.map((field) => (
+          <CustomInput key={field.name} onChange={handleChange} {...field} />
+        ))}
+      <div className="d-grid">
+        <Button type="submit" disabled={isLoading}>
+          {isLoading ? "Adding book..." : "Add Book"}
+        </Button>
+      </div>
     </Form>
   );
 };
