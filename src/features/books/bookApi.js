6a64f3773106ed9735a7f22431437032dd28c6d5
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-02 17:10:18 +0545

    added api method to post new book

--- src/features/books/bookApi.js
+++ src/features/books/bookApi.js
@@ -1,7 +1,7 @@
 import { apiProcessor } from "../../services/apiService";
 
 
-const apiUrl = import.meta.env.VITE_API_URL+"/book";
+const apiUrl = import.meta.env.VITE_API_URL+"/books";
 
 export const fetchAllBookApi = async () => {
   const response = await apiProcessor({
@@ -19,6 +19,17 @@ export const fetchAvailableBookApi = async () => {
     method: "get",
     showToast:false,
     isPrivate: true,
+  });
+  return response;
+};
+
+export const postNewBookApi = async (payload) => {
+  const response = await apiProcessor({
+    url: `${apiUrl}`,
+    method: "post",
+    showToast:true,
+    isPrivate: true,
+    data: payload
   });
   return response;
 };
