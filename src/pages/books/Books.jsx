9137675bae2ac3317c4ea0f6fa588bd50fe80b4f
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-10 17:29:45 +0545

    added book table component and styled book page and also made simple ui

--- src/pages/books/Books.jsx
+++ src/pages/books/Books.jsx
@@ -1,7 +1,34 @@
-const Books = () => {
-  return (
-    <div>Books</div>
-  )
-}
+import BooksTable from "@components/tables/BookTable";
+import { fetchAllBookAction } from "@features/books/bookAction";
+import { useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 
-export default Books
+const Books = () => {
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(fetchAllBookAction());
+  },[dispatch])
+  return (
+    <div className="px-3 py-2">
+      <h1>Books</h1>
+      <hr />
+      <div className="text-end">
+        <Button>Add New Book</Button>
+      </div>
+      <div className="mt-4">
+        <div className="d-flex flex-row justify-content-between px-4">
+          <span>10 Book(s) found</span>
+          <div>
+            <Form.Control placeholder="Search book by name..." />
+          </div>
+        </div>
+        <div>
+          <BooksTable />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Books;
