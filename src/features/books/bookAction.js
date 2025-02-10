de09b013a65718039c0b32a43283b40bd168df8b
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-10 17:29:00 +0545

    consoled the incoming books data and removed id check

--- src/features/books/bookAction.js
+++ src/features/books/bookAction.js
@@ -5,9 +5,10 @@ import { setBooks } from "./bookSlice";
 export const fetchAllBookAction = () => async (dispatch) => {
   // call api
   const { status, data } = await fetchAllBookApi();
+  console.log(data);
   //receive book
   // dispatch book to redux store
-  status === "success" && data?._id && dispatch(setBooks(data));
+  status === "success" && dispatch(setBooks(data));
 };
 export const fetchAvailableBookAction = () => async (dispatch) => {
   // call api
