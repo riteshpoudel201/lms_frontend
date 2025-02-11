cd5d93c31b02a6c0093baf127f3b4a0ce99cc513
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-11 17:45:06 +0545

    made table and relevant element dynamic

--- src/pages/books/Books.jsx
+++ src/pages/books/Books.jsx
@@ -1,30 +1,55 @@
 import BooksTable from "@components/tables/BookTable";
 import { fetchAllBookAction } from "@features/books/bookAction";
-import { useEffect } from "react";
+import { Plus } from "lucide-react";
+import { useEffect, useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Books = () => {
+  const { book } = useSelector((state) => state.bookInfo);
+  const [filterBook, setFilterBook] = useState(book ?? []);
   const dispatch = useDispatch();
-  useEffect(()=>{
+
+  const handleSearchChange = (e) => {
+    const searchValue = e.target.value.toLowerCase();
+    const filteredBook = book.filter((data) =>
+      data.title.toLowerCase().includes(searchValue)
+    );
+    setFilterBook(filteredBook);
+  };
+
+  useEffect(() => {
+    setFilterBook(book);
+  }, [book]);
+
+  useEffect(() => {
     dispatch(fetchAllBookAction());
-  },[dispatch])
+  }, [dispatch]);
   return (
     <div className="px-3 py-2">
       <h1>Books</h1>
       <hr />
-      <div className="text-end">
-        <Button>Add New Book</Button>
+      <div className="d-flex flex-row justify-content-end">
+        <Link to="/user/new-book">
+          <Button className="d-flex flex-row gap-2">
+            <Plus />
+            Add New Book
+          </Button>
+        </Link>
       </div>
       <div className="mt-4">
         <div className="d-flex flex-row justify-content-between px-4">
-          <span>10 Book(s) found</span>
+          <span>{filterBook?.length ?? 0} Book(s) found</span>
           <div>
-            <Form.Control placeholder="Search book by name..." />
+            <Form.Control
+              placeholder="Search book by name..."
+              onChange={handleSearchChange}
+            />
           </div>
         </div>
-        <div>
-          <BooksTable />
+        <div className="mt-4">
+          <BooksTable data={filterBook} />
         </div>
       </div>
     </div>
