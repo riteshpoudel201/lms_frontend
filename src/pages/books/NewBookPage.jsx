7bb209533fe55c77e2f140fe89011df8363af08f
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-11 17:45:27 +0545

    added back button to navigate back to book page

--- src/pages/books/NewBookPage.jsx
+++ src/pages/books/NewBookPage.jsx
@@ -1,9 +1,11 @@
 import { NewBookForm } from "@components/forms"
+import { ArrowLeft } from "lucide-react"
+import { Link } from "react-router-dom"
 
 const NewBookPage = () => {
   return (
-    <div className="m-2">
-      <h3 className="mb-4">Insert New Book Details</h3>
+    <div className="m-2 mx-4">
+      <h3 className="mb-4 d-flex flex-row gap-4"><Link to="/user/books" className="text-dark"><ArrowLeft /></Link>Insert New Book Details</h3>
       <NewBookForm />
     </div>
   )
