eeb45ac42d07b5fffac441380279ba44235fa532
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-13 18:03:29 +0545

    added form for editing book page

--- src/pages/books/EditBookPage.jsx
+++ src/pages/books/EditBookPage.jsx
@@ -1,7 +1,13 @@
+import { EditBookForm } from "@components/forms"
+import { ArrowLeft } from "lucide-react"
+import { Link } from "react-router-dom"
 
 const EditBookPage = () => {
   return (
-    <div>EditBookPage</div>
+    <div className="m-2 mx-4">
+      <h3 className="mb-4 d-flex flex-row gap-4"><Link to="/user/books" className="text-dark"><ArrowLeft /></Link>Update Book Details</h3>
+      <EditBookForm data={[]}/>
+    </div>
   )
 }
 
