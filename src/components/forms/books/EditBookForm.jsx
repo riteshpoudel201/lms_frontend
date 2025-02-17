ce7956c138317385898e608ea3ba15da11855074
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-17 23:20:18 +0545

    update form pre-filled via id

--- src/components/forms/books/EditBookForm.jsx
+++ src/components/forms/books/EditBookForm.jsx
@@ -1,29 +1,76 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable react-hooks/exhaustive-deps */
 import CustomInput from "@components/common/CustomInput";
 import useForm from "@hooks/useForm";
 import { Button, Form } from "react-bootstrap";
 import { bookFields } from "@/assets/custom-inputs/bookInputs";
-import { postNewBookAction } from "@features/books/bookAction";
-import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+// import { postNewBookAction } from "@features/books/bookAction";
+// import { useDispatch } from "react-redux";
 
-const EditBookForm = ({data}) => {
-  const { formData, handleChange, isLoading, setIsLoading } = useForm({});
+const EditBookForm = () => {
+  const { formData, setFormData, handleChange, isLoading, setIsLoading } =
+    useForm({});
+  const { books } = useSelector((state) => state.bookInfo);
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-  const dispatch = useDispatch();
-
+  // const dispatch = useDispatch();
+  console.log("Type of Book: ", typeof books);
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
-    dispatch(postNewBookAction(formData));
+    // dispatch(postNewBookAction(formData));
+    console.log(formData);
     setIsLoading(false);
   };
-  console.log(data);
+  useEffect(() => {
+    console.log("I am inside use effect.");
+    if (id !== formData?._id) {
+      console.log("Inside id check.");
+      const selectedBook = books?.find((book) => book._id === id);
+      console.log("Selected Book: ", selectedBook);
+      setFormData(selectedBook);
+    } else {
+      navigate("/user/books");
+    }
+  }, [id, navigate, setFormData]);
+  console.log("Form data:", formData);
+  console.log("Book fields: ", bookFields);
   return (
     <Form onSubmit={handleSubmit}>
+      <Form.Check
+        name="status"
+        type="switch"
+        id="custom-switch"
+        label={formData?.status.toUpperCase() || "Status"}
+        checked={formData?.status === "active"}
+        onChange={handleChange}
+      />
       {bookFields &&
         bookFields.map((field) => (
-          <CustomInput key={field.name} onChange={handleChange} {...field} />
+          <CustomInput
+            key={field.name}
+            onChange={handleChange}
+            value={formData?.[field.name] || ""}
+            {...field}
+          />
         ))}
+      <div className="container p-4 bg-light rounded shadow-sm mb-3">
+        <hr />
+        <h4 className="fw-bold mb-3 text-dark">Additional Info</h4>
+        <div className="mb-2">
+          <strong>Added By:</strong> {formData?.addedBy?.name || "N/A"} <br />
+          <strong>Date:</strong> {formData?.createdAt || "N/A"}
+        </div>
+        <div className="mb-2">
+          <strong>Last Updated By:</strong> {formData?.lastUpdatedBy?.name || "N/A"}{" "}
+          <br />
+          <strong>Date:</strong> {formData?.updatedAt || "N/A"}
+        </div>
+      </div>
+
       <div className="d-grid">
         <Button type="submit" disabled={isLoading}>
           {isLoading ? "Updating book..." : "Update Book"}
