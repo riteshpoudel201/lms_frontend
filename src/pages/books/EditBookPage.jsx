463b470d815a898d92b3ce69701ac3abb16ca934
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-17 23:21:14 +0545

    remove data fetch logic

--- src/pages/books/EditBookPage.jsx
+++ src/pages/books/EditBookPage.jsx
@@ -1,14 +1,20 @@
-import { EditBookForm } from "@components/forms"
-import { ArrowLeft } from "lucide-react"
-import { Link } from "react-router-dom"
+import { EditBookForm } from "@components/forms";
+import { ArrowLeft } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const EditBookPage = () => {
+
   return (
     <div className="m-2 mx-4">
-      <h3 className="mb-4 d-flex flex-row gap-4"><Link to="/user/books" className="text-dark"><ArrowLeft /></Link>Update Book Details</h3>
-      <EditBookForm data={[]}/>
+      <h3 className="mb-4 d-flex flex-row gap-4">
+        <Link to="/user/books" className="text-dark">
+          <ArrowLeft />
+        </Link>
+        Update Book Details
+      </h3>
+      <EditBookForm />
     </div>
-  )
-}
+  );
+};
 
-export default EditBookPage
+export default EditBookPage;
