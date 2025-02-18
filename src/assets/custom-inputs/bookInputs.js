2143a89455ccd65031085f5d8932238cff6bf65e
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-18 19:35:38 +0545

    added input fields for updating book

--- src/assets/custom-inputs/bookInputs.js
+++ src/assets/custom-inputs/bookInputs.js
@@ -1,4 +1,4 @@
-export const bookFields = [
+export const newFormBookFields = [
     {
       label: "Title",
       name: "title",
@@ -53,4 +53,76 @@ export const bookFields = [
       required: true,
       placeholder: "Book Summary"
     }
+  ];
+
+export const updateFormBookFields = [
+    {
+      label: "Title",
+      name: "title",
+      type: "text",
+      required: true,
+      placeholder: "Eg: The Great Tale",
+      minLength: 3
+    },
+    {
+      label: "Slug",
+      name: "slug",
+      type: "text",
+      required: true,
+      placeholder: "Eg: The Great Tale",
+      minLength: 3,
+      disabled: true,
+    },
+    {
+      label: "Year",
+      name: "year",
+      type: "number",
+      required: true,
+      placeholder: "Eg: 2025",
+      min:1904,
+      max: new Date().getFullYear(),
+    },
+    {
+      label: "Image URL",
+      name: "imageURL",
+      type: "url",
+      required: true,
+      placeholder: "Eg: https://www.example.com/images/some-random-pic.jpg"
+    },
+    {
+      label: "ISBN",
+      name: "isbn",
+      type: "number",
+      placeholder:1234567890,
+      required: true,
+      disabled: true,
+    },
+    {
+      label: "Genre",
+      name: "genre",
+      type: "text",
+      required: true,
+      placeholder: "Eg: Action"
+    },
+    {
+      label: "Author",
+      name: "author",
+      type: "text",
+      required: true,
+      placeholder: "Eg: William Shakespeare"
+    },
+    {
+      label: "Description",
+      name: "description",
+      type:"text",
+      as:"textarea",
+      rows:"5",
+      required: true,
+      placeholder: "Book Summary"
+    },
+    {
+      label: "Expected Available Date",
+      name: "expectedAvailabilityDate",
+      type: "date",
+    },
   ];
