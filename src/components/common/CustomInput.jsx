9ef154f4cffc410c7918b68bbfd35d4c275967e3
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-18 19:36:19 +0545

    added date type to show only date excluding timestamps

--- src/components/common/CustomInput.jsx
+++ src/components/common/CustomInput.jsx
@@ -2,8 +2,18 @@
 import Form from "react-bootstrap/Form";
 import { EyeIcon } from "lucide-react";
 import React, { useState } from "react";
-const CustomInput = ({ label, name, text, required, type, ...rest }, ref) => {
+
+const CustomInput = ({ label, name, text, required, type, value, ...rest }, ref) => {
   const [toggle, setToggle] = useState(false);
+  let valueDate = value;
+
+  if (type === "date") {
+    // Check if the value is a valid date or a string in a different format (e.g., ISO string)
+    const formattedDate =
+      value && !isNaN(new Date(value)) ? new Date(value).toISOString().split("T")[0] : '';
+    valueDate = formattedDate || ''; // default to empty string if invalid or no value
+  }
+
   return (
     <Form.Group className="mb-3" controlId={name}>
       <Form.Label className="fs-6">
@@ -16,16 +26,17 @@ const CustomInput = ({ label, name, text, required, type, ...rest }, ref) => {
           name={name}
           ref={ref}
           required={required}
+          value={valueDate}
           {...rest}
         />
         {type === "password" && (
           <EyeIcon
-            onClick={()=> setToggle(prev => !prev)}
+            onClick={() => setToggle((prev) => !prev)}
             style={{
               position: "absolute",
               zIndex: "10",
               right: "0.5rem",
-              top: "20%",
+              top: "20%",  // You may want to adjust this value
               color: "gray",
             }}
           />
