d9cf69f5fbf2e178ec6572cbbfd5e6002d058418
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-27 17:34:56 +0545

    added image handling and appended and sent to backend  form data

--- src/components/forms/books/NewBookForm.jsx
+++ src/components/forms/books/NewBookForm.jsx
@@ -4,23 +4,33 @@ import { Button, Form } from "react-bootstrap";
 import { newFormBookFields } from "@/assets/custom-inputs/bookInputs";
 import { postNewBookAction } from "@features/books/bookAction";
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 const NewBookForm = () => {
   const { formData, handleChange, isLoading, setIsLoading } = useForm({});
+  const [image, setImage] = useState(null);
 
   const dispatch = useDispatch();
+  const handleImageChange = (e) =>{
+setImage(e.target.files[0])
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const bookFormData = new FormData();
+    for(const key in formData){
+      bookFormData.append(key, formData[key]);
+    }
+    bookFormData.append("bookImage", image);
     setIsLoading(true);
-    dispatch(postNewBookAction(formData));
+    dispatch(postNewBookAction(bookFormData));
     setIsLoading(false);
   };
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form onSubmit={handleSubmit} encType="multipart/form-data">
       {newFormBookFields &&
         newFormBookFields.map((field) => (
-          <CustomInput key={field.name} onChange={handleChange} {...field} />
+          <CustomInput key={field.name} onChange={field.type === "file" ? handleImageChange : handleChange} {...field} />
         ))}
       <div className="d-grid">
         <Button type="submit" disabled={isLoading}>
