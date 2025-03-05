8057fd0f957f750c0a379c5b8e67bed0d07e75e7
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-03-05 19:35:03 +0545

    displayed uploaded image list and also added set thumbnail button for making another image thumbnail

--- src/components/forms/books/EditBookForm.jsx
+++ src/components/forms/books/EditBookForm.jsx
@@ -14,6 +14,7 @@ const EditBookForm = () => {
     useForm({});
   const { books } = useSelector((state) => state.bookInfo);
   const [image, setImage] = useState(null);
+  const [thumbnail, setThumbnail] = useState(formData?.imageURL ?? "");
 
   const { id } = useParams();
   const navigate = useNavigate();
@@ -21,9 +22,9 @@ const EditBookForm = () => {
 
   const handleImageChange = (e) => {
     console.log(e.target.files);
-    if(e.target.files.length > 2){
+    if (e.target.files.length > 2) {
       e.target.value = "";
-      return alert("Maximum 2 images are allowed.")
+      return alert("Maximum 2 images are allowed.");
     }
     setImage([...e.target.files]);
   };
@@ -36,6 +37,8 @@ const EditBookForm = () => {
       createdAt,
       updatedAt,
       lastUpdatedBy,
+      imageURL,
+      imageList,
       __v,
       _id: bookId,
       slug,
@@ -47,8 +50,9 @@ const EditBookForm = () => {
     for (const key in rest) {
       bookFormData.append(key, rest[key]);
     }
-    image.map(img=> bookFormData.append("bookImage", img));
-    console.log(bookId, rest);
+    image?.map((img) => bookFormData.append("bookImage", img));
+    imageList?.map((img) => bookFormData.append("imageList", img));
+    bookFormData.append("imageURL", thumbnail);
     dispatch(updateBookAction(bookFormData, bookId));
 
     setIsLoading(false);
@@ -63,7 +67,6 @@ const EditBookForm = () => {
       navigate("/user/books");
     }
   }, [id, navigate, setFormData]);
-  console.log("Form Data: ", formData);
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Check
@@ -83,6 +86,45 @@ const EditBookForm = () => {
             {...field}
           />
         ))}
+      
+      
+
+      <div className="d-flex flex-column gap-3 w-100 mb-3">
+        <h4 className="fw-bold">Available Images:</h4>
+        <div className="d-flex flex-wrap gap-3">
+          {formData?.imageList &&
+            formData.imageList.map((image) => (
+              <div
+                key={image}
+                className="position-relative shadow rounded overflow-hidden"
+                style={{ width: "220px" }}
+              >
+                <div className="position-relative">
+                  <img
+                    src={`${import.meta.env.VITE_API_BASE_URL}/${image}`}
+                    alt="Image Unavailable"
+                    className="w-100 h-100 object-fit-cover rounded"
+                    style={{ height: "200px" }}
+                  />
+                  <div
+                    className="position-absolute top-0 start-0 w-100 h-100 d-flex justify-content-center align-items-center opacity-0 hover-overlay"
+                    style={{
+                      background: "rgba(0, 0, 0, 0.5)",
+                      transition: "0.3s",
+                    }}
+                  >
+                    <Button variant="light" onClick={() => setThumbnail(image)}>
+                      Set Thumbnail
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+
+      
+
       <CustomInput
         label="Upload More (Max 2)"
         type="file"
@@ -93,11 +135,11 @@ const EditBookForm = () => {
       {formData?.imageURL && (
         <div className="m-3">
           <img
-            src={`${import.meta.env.VITE_API_BASE_URL}/${formData?.imageURL}`}
+            src={`${import.meta.env.VITE_API_BASE_URL}/${thumbnail || formData?.imageURL}`}
             alt="Image Unavailable"
             style={{
-              height: "200px",
-              width: "200px",
+              height: "100px",
+              width: "100px",
               objectFit: "cover",
               objectPosition: "center",
               borderRadius: "8px",
