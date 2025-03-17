f54e6ab5e3f52b251081f61a4dde59db746b8745
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-03-17 19:22:22 +0545

    applied margin button

--- src/components/pageSection/SectionTitle.jsx
+++ src/components/pageSection/SectionTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SectionTitle = ({title}) => {
   return (
-    <div className='d-flex flex-row align-items-center gap-3'>
+    <div className='d-flex flex-row align-items-center gap-3 mb-3'>
         <h3 style={{textWrap:"nowrap"}}>{title}</h3>
         <div style={{width:"100%", height:"1.9px", backgroundColor:"#000"}}></div>
     </div>
