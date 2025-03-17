a2f4af6a99cc15afb7babc872a745afa8b128dec
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-03-17 19:21:27 +0545

    modified card to make it responsive and also accept related props

--- src/components/pageSection/SectionCard.jsx
+++ src/components/pageSection/SectionCard.jsx
@@ -1,18 +1,27 @@
 import { CardBody, CardImg, CardText, CardTitle } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
 
-const SectionCard = () => {
+const SectionCard = ({imageURL, title, author, year, slug}) => {
   return (
-    <Card style={{ width: "18rem", height:"25rem" }}>
-      <CardImg variant="top" src="/background.jpg" style={{height:"50%"}}/>
-      <CardBody style={{height:"50%"}}>
-        <CardTitle>Card Title</CardTitle>
-        <CardText>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </CardText>
-        <Button variant="primary">Go somewhere</Button>
+    <Card className="mx-auto shadow-lg" style={{ maxWidth: "100%", minWidth: "250px" }}>
+      <CardImg
+        variant="top"
+        src={imageURL ?? "/background.jpg"}
+        className="img-fluid"
+        style={{ height: "180px", objectFit: "cover" }} // Ensures image isn't too long
+      />
+      <CardBody className="d-flex flex-column justify-content-between">
+        <div>
+          <CardTitle className="fs-5">{title || "Card Title"}</CardTitle>
+          <CardText className="text-muted">
+            {author || "No Author"}-{year || "N/A"}
+          </CardText>
+        </div>
+        <Link to={slug}>
+        <Button variant="dark" className="w-100">View Details</Button>
+        </Link>
       </CardBody>
     </Card>
   );
