756628ad87fe5c0c63f608ea33eb0fa5947489f6
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-03-17 19:21:45 +0545

    created custom section container

---
+++ src/components/pageSection/SectionContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SectionContainer = ({children}) => {
+  return (
+    <div className='mt-4'>
+      {children}
+    </div>
+  )
+}
+
+export default SectionContainer
