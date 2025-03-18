022b0ed299cbd67309078666e2030365c30358d8
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-03-18 19:11:14 +0545

    added publicly accessible book to redux state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import { ToastContainer } from "react-toastify";
 import AppRoutes from "./routes/AppRoutes";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchAvailableBookAction } from "@features/books/bookAction";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    //fetch available books to the client.
+    console.log("Inside app.jsx use effect.")
+    dispatch(fetchAvailableBookAction());
+  }, [dispatch]);
   return (
     <>
       <AppRoutes />
