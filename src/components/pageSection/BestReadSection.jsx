ce242c87aafaeca698e68b7174e7d3418576c815
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-03-18 19:11:38 +0545

    filtered book to show top 4 books according to date or rating

--- src/components/pageSection/BestReadSection.jsx
+++ src/components/pageSection/BestReadSection.jsx
@@ -3,16 +3,24 @@ import SectionContent from "./SectionContent";
 import SectionTitle from "./SectionTitle";
 import SectionCard from "./SectionCard";
 import SectionContainer from "./SectionContainer";
+import { useSelector } from "react-redux";
 
 const BestReadSection = () => {
+  const { publicBooks } = useSelector((state) => state.bookInfo);
+  let books = [];
+  if (publicBooks.length) {
+    const sorted = [...publicBooks].sort(
+      (a, b) => b.averageRating - a.averageRating
+    );
+    books = sorted.slice(0, 4);
+  }
   return (
     <SectionContainer>
       <SectionTitle title="Best Read" />
       <SectionContent>
-        <SectionCard />
-        <SectionCard />
-        <SectionCard />
-        <SectionCard />
+        {books.map((book) => (
+          <SectionCard key={book._id} {...book} />
+        ))}
       </SectionContent>
     </SectionContainer>
   );
--- src/components/pageSection/JustInSection.jsx
+++ src/components/pageSection/JustInSection.jsx
@@ -3,17 +3,32 @@ import SectionContent from './SectionContent'
 import SectionTitle from './SectionTitle'
 import SectionCard from './SectionCard'
 import SectionContainer from './SectionContainer'
+import { useSelector } from 'react-redux'
 
 const JustInSection = () => {
+  const {publicBooks} = useSelector((state)=> state.bookInfo)
+  let books=[];
+  if(publicBooks.length){
+    const sorted  = [...publicBooks].sort((a,b)=> new Date(a.createdAt)-new Date(b.createdAt))
+    books = sorted.slice(0,4);
+  }
+
+  console.log(books);
+
   return (
     <SectionContainer>
       <SectionTitle title="Just In"/>
       <SectionContent>
+        {
+          books.map(book=>(
+            <SectionCard key={book._id} {...book}/>
+          ))
+        }
+        {/* <SectionCard />
         <SectionCard />
         <SectionCard />
         <SectionCard />
-        <SectionCard />
-        <SectionCard />
+        <SectionCard /> */}
       </SectionContent>
     </SectionContainer>
   )
