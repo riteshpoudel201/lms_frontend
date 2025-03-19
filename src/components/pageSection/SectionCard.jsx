6eac2dd9539afb66668f398600d01a42c3df3ae6
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-03-19 19:41:40 +0545

    modified book link url

--- src/components/pageSection/SectionCard.jsx
+++ src/components/pageSection/SectionCard.jsx
@@ -19,7 +19,7 @@ const SectionCard = ({imageURL, title, author, year, slug}) => {
             {author || "No Author"}-{year || "N/A"}
           </CardText>
         </div>
-        <Link to={slug}>
+        <Link to={"book/" +slug}>
         <Button variant="dark" className="w-100">View Details</Button>
         </Link>
       </CardBody>
