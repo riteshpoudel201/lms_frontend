96d2aca9c35d7853d24be3f047727d3c0ea19f0b
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-04-21 19:31:02 +0545

    made searchable

--- src/components/common/SearchBar.jsx
+++ src/components/common/SearchBar.jsx
@@ -1,17 +1,32 @@
+import { useRef } from "react";
 import { Form, InputGroup } from "react-bootstrap";
 import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const SearchBar = () => {
+  const searchRef = useRef(null);
+  const navigate = useNavigate();
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const str = searchRef.current.value;
+    navigate("book?search=" + str);
+  };
   return (
     <div>
-      <InputGroup className="">
-        <Form.Control
-          placeholder="Search book..."
-          aria-label="Search book input"
-          aria-describedby="Search book input field"
-        />
-        <InputGroup.Text id="basic-addon2"><FaSearch /></InputGroup.Text>
-      </InputGroup>
+      <Form onSubmit={handleSearch}>
+        <InputGroup className="">
+          <Form.Control
+            name="search"
+            placeholder="Search book..."
+            aria-label="Search book input"
+            aria-describedby="Search book input field"
+            ref={searchRef}
+          />
+          <InputGroup.Text id="basic-addon2" as="button">
+            <FaSearch />
+          </InputGroup.Text>
+        </InputGroup>
+      </Form>
     </div>
   );
 };
