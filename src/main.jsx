f50de60363006b6b0f704f0b6a92d06e2dba042c
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-05-04 18:54:00 +0545

    added redux persist provider

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,18 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
-import store from "./redux/store.js"
+import store, { persisitor } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persisitor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
