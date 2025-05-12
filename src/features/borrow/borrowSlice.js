d50d5ab766b78e666ddaffe86fdff76f0203f19f
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-05-12 18:28:58 +0545

    added state for recent borrow as well

--- src/features/borrow/borrowSlice.js
+++ src/features/borrow/borrowSlice.js
@@ -2,18 +2,25 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   borrow: [],
+  recentBorrow: [],
 };
 const bookSlice = createSlice({
   name: "borrow",
   initialState,
   reducers: {
     setBorrow: (state, action) => {
-      state.books = action.payload;
+      state.borrow = action.payload;
+    },
+    setRecentBorrow: (state, action) => {
+      state.recentBorrow = action.payload;
+    },
+    clearRecentBorrow: (state) => {
+      state.recentBorrow = [];
     },
   },
 });
 
 const { reducer, actions } = bookSlice;
 
-export const { setBorrow } = actions;
+export const { setBorrow, setRecentBorrow, clearRecentBorrow } = actions;
 export default reducer;
