f1cb2e888f1fcd374708ab1bf836029c83a5864b
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-05-12 18:30:43 +0545

    added borrow reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,10 @@ import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/users/userSlice";
 import bookReducer from "../features/books/bookSlice";
 import cartReducer from "../features/cart/cartSlice";
+import borrowReducer from "../features/borrow/borrowSlice";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import persistStore from "redux-persist/es/persistStore";
-
 const persistConfig = {
   key: "carts",
   storage,
@@ -15,6 +15,7 @@ const rootReducer = combineReducers({
   userInfo: userReducer,
   bookInfo: bookReducer,
   cartInfo: persistReducer(persistConfig, cartReducer),
+  borrowInfo: borrowReducer,
 });
 
 const store = configureStore({
