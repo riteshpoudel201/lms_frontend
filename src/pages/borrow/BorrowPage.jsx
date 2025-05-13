7512ac96bb95d412eef70b2a22134e96d252eb19
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-05-13 21:25:28 +0545

    added tab to switch between all borrowed list and self borrowed list

--- src/pages/borrow/BorrowPage.jsx
+++ src/pages/borrow/BorrowPage.jsx
@@ -1,8 +1,26 @@
+import AllBorrowTable from "@components/tables/AllBorrowTable";
+import SelfBorrowTable from "@components/tables/SelfBorrowTable";
+import { Container, Row, Tab, Tabs } from "react-bootstrap";
 
 const BorrowPage = () => {
   return (
-    <div>BorrowPage</div>
-  )
-}
+    <Container>
+      <Row>
+        <div className="px-3 py-2">
+          <h1> Borrow History</h1>
+          <hr />
+          <Tabs defaultActiveKey="all" className="mb-3" aria-disabled>
+            <Tab eventKey="all" title="All">
+              <AllBorrowTable />
+            </Tab>
+            <Tab eventKey="self" title="Self">
+              <SelfBorrowTable />
+            </Tab>
+          </Tabs>
+        </div>
+      </Row>
+    </Container>
+  );
+};
 
-export default BorrowPage
+export default BorrowPage;
