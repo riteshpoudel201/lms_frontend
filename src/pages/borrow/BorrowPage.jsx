dc3356fc1d6040eb408ef74d836d7c70f51365e8
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-05-14 22:23:14 +0545

    conditionally rendered the tab

--- src/pages/borrow/BorrowPage.jsx
+++ src/pages/borrow/BorrowPage.jsx
@@ -1,18 +1,27 @@
 import AllBorrowTable from "@components/tables/AllBorrowTable";
 import SelfBorrowTable from "@components/tables/SelfBorrowTable";
 import { Container, Row, Tab, Tabs } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 const BorrowPage = () => {
+  const { user } = useSelector((state) => state.userInfo);
+  const isAdmin = user.role === "admin";
   return (
     <Container>
       <Row>
         <div className="px-3 py-2">
           <h1> Borrow History</h1>
           <hr />
-          <Tabs defaultActiveKey="all" className="mb-3" aria-disabled>
-            <Tab eventKey="all" title="All">
-              <AllBorrowTable />
-            </Tab>
+          <Tabs
+            defaultActiveKey={isAdmin ? "all" : "self"}
+            className="mb-3"
+            aria-disabled
+          >
+            {isAdmin && (
+              <Tab eventKey="all" title="All">
+                <AllBorrowTable />
+              </Tab>
+            )}
             <Tab eventKey="self" title="Self">
               <SelfBorrowTable />
             </Tab>
