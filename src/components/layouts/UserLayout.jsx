b928ba0e105d4f0ff4d302601e70ed34ec14deb1
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-05-14 22:22:52 +0545

    shown the loggedin user's name with role

--- src/components/layouts/UserLayout.jsx
+++ src/components/layouts/UserLayout.jsx
@@ -3,15 +3,18 @@ import Footer from "../common/Footer";
 import Header from "../common/Header";
 import Sidebar from "../common/Sidebar";
 import AuthLayout from "./AuthLayout";
+import { useSelector } from "react-redux";
 
 const UserLayout = () => {
+  const {user} = useSelector(state=> state.userInfo)
   return (
     <AuthLayout>
       <Header />
       <div className="w-100 d-flex flex-row">
         <div className="p-3 bg-dark text-white" style={{ width: "15vw" }}>
           <div>Welcome Back</div>
-          <h4>Ritesh Poudel</h4>
+          <h4>{user.firstName + " " +user.lastName}</h4>
+          <h5 style={{textTransform:"capitalize", fontSize:"0.8rem"}}>({user.role})</h5>
           <hr />
           <Sidebar />
         </div>
